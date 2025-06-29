e785332ef089748e7b4ac16c93835970c9153034
Oluwaseun F <pharma4mee@yahoo.com>
2025-06-29 23:02:17 +0100

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+const mobileMenuBtn = document.getElementById("mobile-menu-button")
+const mobileMenu = document.getElementById("mobile-menu")
+
+mobileMenuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden")
+})
