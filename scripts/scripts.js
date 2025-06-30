1b99d8ecbc2b936b47dcef30aed05223abc42052
Oluwaseun F <pharma4mee@yahoo.com>
2025-06-30 08:40:04 +0100

    index page completed, animated properly

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,40 @@
+// Navigation toggle button 
 const mobileMenuBtn = document.getElementById("mobile-menu-button")
 const mobileMenu = document.getElementById("mobile-menu")
 
 mobileMenuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden")
 })
+
+// Lightbox buttons
+const filterButtons = document.querySelectorAll('.filter-btn')
+const projectItems = document.querySelectorAll('.project-item')
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const filter = button.getAttribute('data-filter');
+
+        projectItems.forEach(item => {
+            const category = item.getAttribute('data-category')
+
+            if(filter === 'all' || category === filter) {
+                item.classList.remove('hidden');
+            } else {
+                item.classList.add('hidden');
+            }
+        })
+
+        // Update button active styles
+        filterButtons.forEach(btn => {
+        btn.classList.remove('bg-blue-600', 'text-white');
+        btn.classList.add('bg-gray-200', 'text-gray-700');
+        });
+
+        button.classList.remove('bg-gray-200', 'text-gray-700');
+        button.classList.add('bg-blue-600', 'text-white');
+    });
+
+})
+
+
+
